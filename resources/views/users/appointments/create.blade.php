91b7f546df883dc74166ab49da864e720ea70bf8
Fateme Atefrad <fa.atefrad@gmail.com>
2024-03-25 20:49:10 +0330

    [refactor] ajax js moved to a seperate file

--- resources/views/users/appointments/create.blade.php
+++ resources/views/users/appointments/create.blade.php
@@ -93,7 +93,7 @@
                                 @enderror
                             </div>
 
-                            <div id="time_options_div" class="control-group ml-3 border border-white rounded p-3 d-none">
+                            <div id="time_options_div" class="control-group ml-3 border border-white text-light rounded p-3 d-none">
                             </div>
 
                             <div>
@@ -110,134 +110,10 @@
 @endsection
 
 @section('script')
+
+    <script src="{{ asset('js/time-ajax.js') }}"></script>
+
     <script>
-        const fastestTimeInput = $('#fastest_time_input');
-
-        $('#fastest_time').click(function () {
-
-            let servicesId = '';
-
-             $('input[name="services[]"]:checked').each(function () {
-
-                 servicesId += $(this).val() + ' ';
-             });
-
-            $.ajax({
-                url: "/api/times/show?service_id=" + servicesId,
-                type: "GET",
-                dataType: 'json',
-                success: function (response) {
-
-                    if(response.error)
-                    {
-                        alert(response.error);
-                    }
-
-                    let timesId = '';
-
-                    for(let timeSlot of response)
-                    {
-                        timesId+= timeSlot.id + ',';
-                    }
-
-                    fastestTimeInput.val(timesId);
-                    fastestTimeInput.removeClass('d-none');
-                    fastestTimeInput.addClass('d-block');
-                    $('#fastest_time_label').text(
-                        response[0].year + '-' +
-                        response[0].month + '-' +
-                        response[0].day + ' -- ' +
-                        response[0].start_time.substring(0,5) + '-' +
-                        response[response.length - 1].finish_time.substring(0,5)
-                    );
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    alert("Status: " + textStatus);
-                    alert("Error: " + errorThrown);
-                }
-            });
-
-            if(!$('#time_options_div').hasClass('d-none'))
-            {
-                $('#time_options_div').addClass('d-none');
-            }
-        });
-
-        //ajax for all available time slots
-        $('#custom_time').click(function () {
-
-            let servicesId = '';
-
-            $('input[name="services[]"]:checked').each(function () {
-
-                servicesId += $(this).val() + ' ';
-            });
-
-            $.ajax({
-                url: "/api/times/index?service_id=" + servicesId,
-                type: "GET",
-                dataType: 'json',
-                success: function (response) {
-
-                    if(response.error)
-                    {
-                        alert(response.error);
-                    }
-
-                    let html = '<div id="time_options">';
-
-                    let i = 1;
-
-                    let timesId;
-
-                    // let firstDay = response[0].day;
-
-                    for (let timeSlots of response) {
-                        if ($.isArray(timeSlots)) {
-
-                            timesId = '';
-
-                            for (let timeSlot of timeSlots) {
-                                timesId += timeSlot.id + ',';
-
-                            }
-                        } else {
-
-                            timesId = timeSlots.id;
-
-                            timeSlots[0] = timeSlots;
-                            timeSlots[timeSlots.length - 1] = timeSlots
-
-                        }
-
-                        // html += '<div class="col-lg-6">';
-                        html += '<input type="radio" class="form-check-input check-box" name="time" id="timeSlot-' + i + '"' + 'value="' + timesId + '" required="required" />';
-                        html += '<label class="form-check-label check-box-label d-block text-light" for="timeSlot-' + i + '">';
-                        html += timeSlots[0].year + '-' +
-                            timeSlots[0].month + '-' +
-                            timeSlots[0].day + ' -- ' +
-                            timeSlots[0].start_time.substring(0, 5) + '-' +
-                            timeSlots[timeSlots.length - 1].finish_time.substring(0, 5);
-                        html += '</label>';
-                        // html += '</div>';
-
-                        i++;
-                    }
-
-                    html += '</div>';
-
-                    $('#time_options').remove();
-
-                    $('#time_options_div').append(html);
-
-                    $('#time_options_div').removeClass('d-none');
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    alert("Status: " + textStatus);
-                    alert("Error: " + errorThrown);
-                }
-            });
-        });
 
         setTimeout(function () {
             $('#transaction_error').toggleClass('d-none');
